d57dd8ab554f94ca50b15209e18a846030a49e28
Pablo Navarro <pablonavarroduro@gmail.com>
2024-04-25 13:05:30 +0200

    root commit

---
+++ src/test/java/com/tfc/v1/TrabajoFindeCicloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfc.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoFindeCicloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
