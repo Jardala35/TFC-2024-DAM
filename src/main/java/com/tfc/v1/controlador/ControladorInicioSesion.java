b6d34b430b5748922274df51e0e24c294ba7e8c0
Sergio Rubio Núñez <Sergio Rubio Nez@DESKTOP-KNS91BO>
2024-05-12 12:52:58 +0200

    Librerias y controlador con vista

---
+++ src/main/java/com/tfc/v1/controlador/ControladorInicioSesion.java
@@ -0,0 +1,35 @@
+package com.tfc.v1.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ControladorInicioSesion implements Initializable {
+
+    @FXML
+    private TextField usuarioTextField;
+
+    @FXML
+    private TextField contrasenaTextField;
+
+    @FXML
+    private Button iniciarSesionButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Inicialización del controlador, se llama automáticamente después de cargar la vista
+    }
+
+    @FXML
+    private void handleIniciarSesion() {
+        String usuario = usuarioTextField.getText();
+        String contrasena = contrasenaTextField.getText();
+
+   
+        System.out.println("Iniciando sesión para usuario: " + usuario);
+    }
+
+}
